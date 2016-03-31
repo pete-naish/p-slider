7a22930f1d624bb228534841eb2a0c8db0fc8e91
Pete Naish <petenaish@petenaish.ddb-europe.local>
2016-03-31 18:09:53 +0100

    add dots and hook them up

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 var pSlider = (function(window, undefined) {
-    var slides,
-        controller,
+    var controller,
         wipeAnimation,
-        scene,
-        scrolling = false;
+        scene;
 
     var ui = {
         el: '.p-slider',
         skipSlides: null,
-        slides: []
+        slides: null,
+        sliderControls: null,
+        slideAnchors: null
     };
 
     var state = {
-        currentSlide: {}
+        currentSlide: null
     }
 
     function init() {
@@ -20,21 +20,27 @@ var pSlider = (function(window, undefined) {
         initController();
         initAnimation();
         initScene();
-        // bindEvents();
+        bindEvents();
     }
 
     function initUI() {
-        slides = $(ui.el).find('.p-slider__slide');
-
-        slides.each(function(i, slide) {
-            ui.slides[i] = slide;
-            $(slide).attr('id', 'slide' + i);
-        });
-
+        ui.slides = $(ui.el).find('.p-slider__slide');
         ui.skipSlides = $(ui.el).find('.p-slider__skip-slides');
+        ui.sliderControls = $(ui.el).find('.p-slider__controls');
+
+        buildSlideAnchors();
 
         updateActiveSlide(ui.slides[0]);
-        // console.log('ui', ui);
+    }
+
+    function buildSlideAnchors() {
+        $.each(ui.slides, function(i) {
+            var listItem = $('<li class="p-slider__dot"><button class="p-slider__dot-button">' + i + '</button></li>');
+
+            ui.sliderControls.append(listItem);
+        });
+
+        ui.slideAnchors = $(ui.sliderControls).find('.p-slider__dot');
     }
 
     function initController() {
@@ -45,13 +51,10 @@ var pSlider = (function(window, undefined) {
         });
 
         controller.scrollTo(function(pos) {
-            TweenMax.to(window, 5, {
+            TweenMax.to(window, 1, {
                 scrollTo: {
                     y: pos,
-                    // autoKill: true,
-                    onComplete: function() {
-                        console.log('complete');
-                    }
+                    autoKill: true
                 }
             });
         });
@@ -76,42 +79,34 @@ var pSlider = (function(window, undefined) {
     }
 
     function initScene() {
-        // scene = new ScrollMagic.Scene({
-        //     triggerElement: ui.el,
-        //     duration: '100%'
-        // })
-        // .setTween(wipeAnimation)
-        // .setPin(ui.el, {
-        //     pushFollowers: false
-        // })
-        // .addTo(controller);
-
-        $.each(ui.slides, function(i, slide) {
-            new ScrollMagic.Scene({
-               triggerElement: slide 
-            })
-            .setPin(slide)
-            .addTo(controller);
-        });
+        scene = new ScrollMagic.Scene({
+            triggerElement: ui.el,
+            duration: '100%'
+        })
+        .setTween(wipeAnimation)
+        .setPin(ui.el, {
+            pushFollowers: false
+        })
+        .addTo(controller);
     }
 
     function bindEvents() {
         scene.on('progress', function (event) {
-            console.log(event.state);
             var progress = event.progress.toFixed(2);
             var direction = controller.info('scrollDirection');
             var slide1 = progress > 0.15 && progress < 0.33;
             var slide2 = progress > 0.48 && progress < 0.66;
+            var newPos;
             // var slide3 = progress > 0.63 && progress < 0.66;
-            // console.log(progress);
-            // console.log(event.scrollDirection);
-            // console.log('scrollPos', controller.info('scrollPos'));
 
             if (direction === 'FORWARD') {
                 if (slide1) { // first slide
-                    // controller.scrollTo('#' + ui.slides[1].id);
+                    newPos = ($(window).height() / ui.slides.length) * 1;
+                    controller.scrollTo(newPos);
                     // scene.progress(0.33);
                 } else if (slide2) {
+                    newPos = ($(window).height() / ui.slides.length) * 2;
+                    controller.scrollTo(newPos);
                     // controller.scrollTo('#' + ui.slides[2].id);
                 }
             } else if (direction === 'REVERSE') {
@@ -134,29 +129,35 @@ var pSlider = (function(window, undefined) {
             }, 2000);
         });
 
-        // state.currentSlide.on('updateActiveSlide', function() {
-        //     console.log('updated');
-        // });
+        ui.slideAnchors.on('click.navigateToSlide', function(e) {
+            var slideNum = $(this).text();
+            var newPos = ($(window).height() / ui.slides.length) * slideNum;            
+
+            e.preventDefault();
+            controller.scrollTo(newPos);
+        });
     }
 
     function updateActiveSlide(slide) {
+        var slideIndex = $(slide).index();
+
         state.currentSlide = slide;
 
-        $(state.currentSlide).trigger('updateActiveSlide');
-
-        slides.removeClass('is-active');
-        $(slide).addClass('is-active');
+        $(ui.slides).add(ui.slideAnchors).removeClass('is-active');
+        $(state.currentSlide).add(ui.slideAnchors[slideIndex]).addClass('is-active');
     }
 
     function endSlider() {
         state.currentSlide = null;
 
-        slides.removeClass('is-active');
+        $(ui.slides).add(ui.slideAnchors).removeClass('is-active');
+
         TweenMax.to(ui.skipSlides, '.5', {bottom: - ui.skipSlides.outerHeight()});
     }
 
     return {
-        init: init
+        init: init,
+        ui: ui
     }
 })(window);
 
